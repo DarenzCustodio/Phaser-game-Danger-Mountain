fe78fe8538a1493eb3be8e213dba4333aec5aa58
Ben04051 <benmitchell0405@hotmail.co.uk>
2024-02-27 16:03:31 +0000

    corrected loading screen to work

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -9,14 +9,19 @@ export class Preloader extends Scene
 
     init ()
     {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
+
 
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(1500, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+
+        const loadingText = this.add.text(1500, 300, 'Loading...', {
+            fontFamily: 'Arial',
+            fontSize: 40,
+            color: '#ffffff'
+        }).setOrigin(0.5);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(1500-230, 384, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress: number) => {
